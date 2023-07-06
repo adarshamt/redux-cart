aaed8133f9bc6b5a9d80d70fd400d2d9bd327c63
adarshamt <adarshamt@gmail.com>
2023-07-06 16:21:35 +0530

    count

--- src/componets/productLIst/CartButtons/AfterCart.js
+++ src/componets/productLIst/CartButtons/AfterCart.js
@@ -4,14 +4,16 @@ import { increment,decrement } from '../../redux/cart'
 import { useDispatch,useSelector } from 'react-redux'
 
 
-const AfterCart = ({cartCount}) => {
+const AfterCart = () => {
+
+  const count = useSelector((state)=> state.cart.cartCount)
 
   const dispatch = useDispatch()
   return (
     <div className='after-cart'>
-        <button className='cart-counter-button'onClick={()=>dispatch(decrement)}>-</button>
-        <div className='counter'> {cartCount}</div>
-        <button className='cart-counter-button' onClick={()=>dispatch(increment)}>+</button>
+        <button className='cart-counter-button'onClick={()=>dispatch(decrement())}>-</button>
+        <div className='counter' style={{margin:'10px'}}> {count}</div>
+        <button className='cart-counter-button' onClick={()=>dispatch(increment())}>+</button>
 
 
 
